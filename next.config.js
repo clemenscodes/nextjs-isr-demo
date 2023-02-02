69e6c5ac8c6c925213f7d3e95d86a40ebdd018e8
Clemens Horn <clemens.horn@mni.thm.de>
2023-02-03 00:58:41 +0100

    no inmemory cache

--- next.config.js
+++ next.config.js
@@ -1,15 +1,19 @@
 module.exports = {
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "x-custom-header",
-            value: "my, custom, header, value",
-          },
-        ],
-      },
-    ];
-  },
+    async headers() {
+        return [
+            {
+                source: "/:path*",
+                headers: [
+                    {
+                        key: "x-custom-header",
+                        value: "my, custom, header, value",
+                    },
+                ],
+            },
+        ];
+    },
+    experimental: {
+        // Defaults to 50MB
+        isrMemoryCacheSize: 0,
+    },
 };
